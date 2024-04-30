cb2cc723786ba1cb0f024dcd4b22e1d9ce62b1f6
AbsoluteGibs <zhpskelvinchew@gmail.com>
2024-04-30 15:06:25 +0800

    routing fix attempt 1

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/my-works" element={<MyWorks/>}/>
+        {/* <Route path="/*" element={<NotFound/>}/> */}
       </Routes>
       <header className="App-header">
         
