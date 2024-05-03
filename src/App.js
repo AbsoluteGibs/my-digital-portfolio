ffe1104bbf29c7b4b410b222bf02f02c5b8b6cf6
AbsoluteGibs <zhpskelvinchew@gmail.com>
2024-05-03 19:17:22 +0800

    changing up margin and media queries

--- src/App.js
+++ src/App.js
@@ -9,23 +9,21 @@ import MyWorks from './MyWorks';
 function App() {
   return (
     <div className="App">
-      <div className='App-child'>
-        <BrowserRouter>
-          <Navbar/>
-          <Routes>
-            {/* <Route path="/" element={<Navigate to="/about" replace />}/> */}
-            <Route path="/about" element={<About/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="/my-works" element={<MyWorks/>}/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          {/* <Route path="/" element={<Navigate to="/about" replace />}/> */}
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/my-works" element={<MyWorks/>}/>
 
-            {/* redirect wrong subdomains to about page*/}
-            <Route
-                path="*"
-                element={<Navigate to="/about" replace />}
-            />
-          </Routes>
-        </BrowserRouter>
-      </div>
+          {/* redirect wrong subdomains to about page*/}
+          <Route
+              path="*"
+              element={<Navigate to="/about" replace />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
